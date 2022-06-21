16648aefc7fdda977827869b218d288b7c1e8edd
Glitch (aware-flicker-psychiatrist) <none>
2022-06-21 17:32:54 +0000

    🏪🛀 Checkpoint ./server.js:22221649/3340

--- server.js
+++ server.js
@@ -6,8 +6,40 @@ const server = http.createServer(app);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
-app.use(express.static('/'))
+app.get('/Coin.js', (req, res) => {
+  res.sendFile(__dirname + '/Coin.js');
+});
+app.get('/Level.js', (req, res) => {
+  res.sendFile('/Level.js');
+});
+app.get('/LevelSetupFunction.js', (req, res) => {
+  res.sendFile(__dirname + '/LevelSetupFunction.js');
+});
+app.get('/Line.js', (req, res) => {
+  res.sendFile(__dirname + '/Line.js');
+});
+app.get('/Music.js', (req, res) => {
+  res.sendFile(__dirname + '/Music.js');
+});
+app.get('/Population.js', (req, res) => {
+  res.sendFile(__dirname + '/Population.js');
+});
+app.get('/bullet.js', (req, res) => {
+  res.sendFile(__dirname + '/bullet.js');
+});
+app.get('/Player.js', (req, res) => {
+  res.sendFile(__dirname + '/Player.js');
+});
+app.get('/sketch.js', (req, res) => {
+  res.sendFile(__dirname + '/sketch.js');
+});
+app.get('/Brain.js', (req, res) => {
+  res.sendFile(__dirname + '/Brain.js');
+});
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/libraries', express.static(__dirname + '/libraries'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/sounds', express.static(__dirname + '/sounds'));
 
 server.listen(5000, () => {
   console.log('listening on *:5000');
